543214067a0c83c2b1ff9d467d84e279e120c307
youmari <youmari300@gmail.com>
2022-04-27 07:34:28 +0000

    Set up redux store and react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { Routes, Route } from 'react-router'
+import Greeting from './Greeting';
 import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      header
-    </div>
+    <>
+      <h1>Hello</h1>
+      <Routes>
+        <Route element={<Greeting />} path="/" />
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
