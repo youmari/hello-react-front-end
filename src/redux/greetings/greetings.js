1dc11e5b2d6af8d2625ecb9b4839a37bba4c675e
youmari <youmari300@gmail.com>
2022-04-27 07:38:24 +0000

    Add greeting action, state and reducer

---
+++ src/redux/greetings/greetings.js
@@ -0,0 +1,32 @@
+import Axios from 'axios';
+
+// conts
+const FETCH_GREETING = 'hello-rails-react/greetings/FETCH_GREETING';
+
+// actions
+const fetchGreeting = (payload) => ({
+  type: FETCH_GREETING,
+  payload,
+});
+
+// state
+const initialState = [];
+
+export const fetchGreetingApi = () => async (dispatch) => {
+  const response = await Axios.get('api/v1/message');
+  const greeting = response.data.text;
+  console.log(greeting, 'jere');
+  dispatch(fetchGreeting(greeting));
+};
+
+// reducer
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_GREETING:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export default reducer;
