ae2e74cf083a8988044becb86a87d4f9ae0bd5f9
youmari <youmari300@gmail.com>
2022-04-27 07:35:39 +0000

    Add greeting component

---
+++ src/Greeting.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchGreetingApi } from '../redux/greetings/greetings';
+
+const Greeting = () => {
+  const greeting = useSelector((state) => state.greetingsReducer);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchGreetingApi());
+  }, [dispatch]);
+
+  const refreshPage = () => {
+    window.location.reload(false);
+  };
+  return (
+    <div>
+      <h1>{greeting}</h1>
+      <button onClick={refreshPage}>Get greeting</button>
+    </div>
+  );
+}
+
+export default Greeting;
